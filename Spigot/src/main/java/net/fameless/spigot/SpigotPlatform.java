6a6204df47ebdfceeb1af4bee51c0ad4d985a190
Nicolai <nicolaigreeven@gmail.com>
2025-07-19 19:39:07 +0200

    Fixed spigot listeners not being registered

--- Spigot/src/main/java/net/fameless/spigot/SpigotPlatform.java
+++ Spigot/src/main/java/net/fameless/spigot/SpigotPlatform.java
@@ -2,10 +2,12 @@ package net.fameless.spigot;
 
 import net.fameless.core.BungeeAFK;
 import net.fameless.core.BungeeAFKPlatform;
+import net.fameless.core.handling.AFKHandler;
 import net.fameless.core.handling.Action;
 import net.kyori.adventure.text.Component;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SpigotPlatform extends JavaPlugin implements BungeeAFKPlatform {
@@ -27,6 +29,8 @@ public final class SpigotPlatform extends JavaPlugin implements BungeeAFKPlatfor
         SpigotCommandHandler commandHandler = new SpigotCommandHandler();
         getCommand("bungeeafk").setExecutor(commandHandler);
         getCommand("bungeeafk").setTabCompleter(commandHandler);
+
+        Bukkit.getPluginManager().registerEvents((Listener) BungeeAFK.injector().getInstance(AFKHandler.class), this);
 
         new Metrics(this, 25575);
     }
