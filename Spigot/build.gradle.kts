a4fc1df349f9a87461513fea1fba2135757dee3d
Nicolai <nicolaigreeven@gmail.com>
2025-07-28 20:54:08 +0200

    Added spigot compatibility check gradle task

--- Spigot/build.gradle.kts
+++ Spigot/build.gradle.kts
@@ -44,4 +44,46 @@ tasks {
     jar {
         enabled = false
     }
+
+    register("checkCompatibility") {
+        group = "compatibility-check"
+        description = "Checks compatibility for all supported Spigot versions by compiling against them."
+        dependsOn(mcVersions.map { "compileAgainstSpigot$it".replace(".", "_") })
+    }
 }
+
+val mcVersions = listOf(
+    "1.8.8",
+    "1.9.4",
+    "1.10.2",
+    "1.11.2",
+    "1.12.2",
+    "1.13.2",
+    "1.14.4",
+    "1.15.2",
+    "1.16.5",
+    "1.17.1",
+    "1.18.2",
+    "1.19.4",
+    "1.20.4"
+)
+
+mcVersions.forEach { version ->
+    val taskName = "compileAgainstSpigot$version".replace(".", "_")
+
+    val spigotApi = "org.spigotmc:spigot-api:$version-R0.1-SNAPSHOT"
+
+    configurations.create(taskName + "CompileOnly")
+    dependencies {
+        add(taskName + "CompileOnly", spigotApi)
+    }
+
+    tasks.register<JavaCompile>(taskName) {
+        group = "compatibility-check"
+        description = "Compile against Spigot $version"
+        classpath = configurations.getByName(taskName + "CompileOnly") + sourceSets["main"].compileClasspath
+        source = sourceSets["main"].java
+        destinationDirectory.set(file("build/compat/$version"))
+    }
+}
+
