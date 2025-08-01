137fcd50e1d052e57dbd1779e3752c76de3fbd5a
Nicolai <nicolaigreeven@gmail.com>
2025-08-01 22:17:02 +0200

    Made the config look better

--- core/src/main/java/net/fameless/core/util/YamlUtil.java
+++ core/src/main/java/net/fameless/core/util/YamlUtil.java
@@ -7,41 +7,66 @@ import org.jetbrains.annotations.NotNull;
 public class YamlUtil {
 
     public static @NotNull String generateConfig() {
-        return "lang: " + Caption.getCurrentLanguage().getIdentifier() + "\n" +
-                "\n" +
-                "# Delay after which the warning message is sent to the player (seconds) | Lang entry: \"notification.afk_warning\"\n" +
-                "# e.g., if set to 60, the player will receive a warning message after 1 minute of inactivity\n" +
-                "warning-delay: " + PluginConfig.get().getInt("warning-delay", 60) + "\n" +
-                "\n" +
-                "# Delay after which a player is marked as AFK (seconds)\n" +
-                "# e.g., if set to 600, the player will be marked as AFK after 10 minutes of inactivity\n" +
-                "afk-delay: " + PluginConfig.get().getInt("afk-delay", 600) + "\n" +
-                "\n" +
-                "# Delay after which the action is executed (seconds)\n" +
-                "# e.g., if set to 630, the player will be kicked or connected after 10 minutes and 30 seconds of inactivity\n" +
-                "action-delay: " + PluginConfig.get().getInt("action-delay", 630) + "\n" +
-                "\n" +
-                "# Action to be performed after action delay is reached. Possible values: \"kick\", \"connect\", \"nothing\".\n" +
-                "# \"kick\" - player is kicked from the server\n" +
-                "# \"connect\" - player is connected to the server specified in the \"afk-server-name\" option\n" +
-                "# \"nothing\" - nothing happens\n" +
-                "action: \"" + PluginConfig.get().getString("action", "kick") + "\"\n" +
-                "\n" +
-                "# Name of the server to which the player will be connected if the action is set to \"connect\"\n" +
-                "# If the server does not exist, the action will default to \"kick\"\n" +
-                "# !!! Only available for BungeeCord and Velocity !!!" + "\n" +
-                "afk-server-name: " + PluginConfig.get().getString("afk-server-name", "") + "\n" +
-                "\n" +
-                "# AFK zone configuration\n" +
-                "# If the action is set to \"teleport\", the player will be teleported to this location\n" +
-                "afk-location:\n" +
-                "  world: " + PluginConfig.get().getSection("afk-location").get("world") + "\n" +
-                "  x: " + PluginConfig.get().getSection("afk-location").get("x") + "\n" +
-                "  y: " + PluginConfig.get().getSection("afk-location").get("y") + "\n" +
-                "  z: " + PluginConfig.get().getSection("afk-location").get("z") + "\n" +
-                "\n" +
-                "# Whether to allow bypass of AFK detection for players with the \"afk.bypass\" permission\n" +
-                "allow-bypass: " + PluginConfig.get().getBoolean("allow-bypass", true) + "\n";
+        return """
+            
+            #  ██████╗ ██╗   ██╗███╗   ██╗ ██████╗ ███████╗███████╗ █████╗ ███████╗██╗  ██╗
+            #  ██╔══██╗██║   ██║████╗  ██║██╔════╝ ██╔════╝██╔════╝██╔══██╗██╔════╝██║ ██╔╝
+            #  ██████╔╝██║   ██║██╔██╗ ██║██║  ███╗█████╗  █████╗  ███████║█████╗  █████╔╝
+            #  ██╔══██╗██║   ██║██║╚██╗██║██║   ██║██╔══╝  ██╔══╝  ██╔══██║██╔══╝  ██╔═██╗
+            #  ██████╔╝╚██████╔╝██║ ╚████║╚██████╔╝███████╗███████╗██║  ██║██║     ██║  ██╗
+            #  ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝
+            #  BungeeAFK - AFK detection plugin for BungeeCord, Velocity and Spigot by Fameless9
+
+            # Language used for messages and notifications
+            # Available languages: en, de
+            lang: %s
+
+            # Delay after which the warning message is sent to the player (seconds) | Lang entry: "notification.afk_warning"
+            # e.g., if set to 60, the player will receive a warning message after 1 minute of inactivity
+            warning-delay: %d
+
+            # Delay after which a player is marked as AFK (seconds)
+            # e.g., if set to 600, the player will be marked as AFK after 10 minutes of inactivity
+            afk-delay: %d
+
+            # Delay after which the action is executed (seconds)
+            # e.g., if set to 630, the player will be kicked or connected after 10 minutes and 30 seconds of inactivity
+            action-delay: %d
+
+            # Action to be performed after action delay is reached. Possible values: "kick", "connect", "nothing".
+            # "kick" - player is kicked from the server
+            # "connect" - player is connected to the server specified in the "afk-server-name" option
+            # "nothing" - nothing happens
+            action: "%s"
+
+            # Name of the server to which the player will be connected if the action is set to "connect"
+            # If the server does not exist, the action will default to "kick"
+            # !!! Only available for BungeeCord and Velocity !!!
+            afk-server-name: %s
+
+            # AFK zone configuration
+            # If the action is set to "teleport", the player will be teleported to this location
+            afk-location:
+              world: %s
+              x: %s
+              y: %s
+              z: %s
+
+            # Whether to allow bypass of AFK detection for players with the "afk.bypass" permission
+            allow-bypass: %b
+            """.formatted(
+                Caption.getCurrentLanguage().getIdentifier(),
+                PluginConfig.get().getInt("warning-delay", 60),
+                PluginConfig.get().getInt("afk-delay", 600),
+                PluginConfig.get().getInt("action-delay", 630),
+                PluginConfig.get().getString("action", "kick"),
+                PluginConfig.get().getString("afk-server-name", ""),
+                PluginConfig.get().getSection("afk-location").get("world"),
+                PluginConfig.get().getSection("afk-location").get("x"),
+                PluginConfig.get().getSection("afk-location").get("y"),
+                PluginConfig.get().getSection("afk-location").get("z"),
+                PluginConfig.get().getBoolean("allow-bypass", true)
+        );
     }
 
 }
