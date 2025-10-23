fc50cd14e6ed42a5d41a255a578156471afa7ab4
tavstal <tavstal@gmail.com>
2025-10-23 11:30:40 +0200

    Enhance afk action bar message by adding action taken message

--- core/src/main/java/net/fameless/core/handling/AFKHandler.java
+++ core/src/main/java/net/fameless/core/handling/AFKHandler.java
@@ -343,8 +343,11 @@ public abstract class AFKHandler {
     }
 
     private void sendActionBar(@NotNull BAFKPlayer<?> player) {
-        if (player.getAfkState() == AFKState.AFK || player.getAfkState() == AFKState.ACTION_TAKEN) {
+        if (player.getAfkState() == AFKState.AFK) {
             player.sendActionbar(Caption.of("actionbar.afk"));
+        }
+        else if (player.getAfkState() == AFKState.ACTION_TAKEN) {
+            player.sendActionbar(Caption.of("actionbar.afk-action-taken"));
         }
     }
 
